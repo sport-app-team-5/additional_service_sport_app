3d87c3dbd1625308c8accafd2449612f488dcfa1
Luis Fernando santa <l.santa@uniandes.edu.co>
2024-04-04 19:16:49 -0500

    update env

--- app/config/env.py
+++ app/config/env.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 
 class Env:
-    PROJECT_NAME: str = "Plan SportApp"
+    PROJECT_NAME: str = "Additional Service SportApp"
     PROJECT_VERSION: str = "1.0.0"
     DB_ENGINE: str = "postgresql"
 
@@ -19,9 +19,6 @@ class Env:
     DB_NAME: str = os.getenv("DB_NAME")
     DATABASE_URL = f"{DB_ENGINE}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:" \
                    f"{DB_PORT}/{DB_NAME}"
-    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-    TOPIC_ARN = os.getenv('TOPIC_ARN')
 
 
 env = Env
