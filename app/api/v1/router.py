5fbcba549a456e6f38fbb40eaa84aa60897aef16
cccortesc1 <cc.cortesc1@uniandes.edu.co>
2024-04-09 21:18:49 -0500

    Se ajusta en nombre de la URI de additional Services

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
 from app.api.v1.private import third_party_router 
 
-private_router = APIRouter(prefix="/aditional_service")
+private_router = APIRouter(prefix="/additional_service")
 private_router.include_router(third_party_router)
