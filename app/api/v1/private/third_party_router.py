d33a91cedd30aadb98cafcf9c9a8f717a61847eb
cccortesc1 <cc.cortesc1@uniandes.edu.co>
2024-04-11 21:52:59 -0500

    Se realizan ajuste de autorización y cobertura de código

--- app/api/v1/private/third_party_router.py
+++ app/api/v1/private/third_party_router.py
@@ -13,30 +13,35 @@ authorized = auth_service.authorized
 third_party_router = APIRouter(
     prefix='/third_parties',
     tags=["ThirdParty"],
+    dependencies=[Depends(oauth2_scheme)]
 )
 
 @third_party_router.post("", response_model=ThirdPartyResponseDTO
+                         ,dependencies=[Security(authorized, scopes=[PermissionEnum.READ_USER.code])]
                          , status_code=status.HTTP_201_CREATED)
 async def create_third_party(third_party: ThirdPartyRequestDTO, db: Session = Depends(get_db)):
     third_party_service = ThirdPartyService()
     third_party_created = third_party_service.create_third_party(third_party, db)
     return third_party_created
 
-@third_party_router.get("", response_model=List[ThirdPartyResponseDTO])
+@third_party_router.get("", response_model=List[ThirdPartyResponseDTO],
+                        dependencies=[Security(authorized, scopes=[PermissionEnum.READ_USER.code])])
 async def get_third_parties(db: Session = Depends(get_db)):
     third_party_service = ThirdPartyService()
     third_parties = third_party_service.get_third_parties(db)
     return third_parties
 
 
-@third_party_router.get("/{third_party_id}", response_model=ThirdPartyResponseDTO)
+@third_party_router.get("/{third_party_id}", response_model=ThirdPartyResponseDTO,
+                        dependencies=[Security(authorized, scopes=[PermissionEnum.READ_USER.code])])
 async def get_third_party_by_id(third_party_id: int = Path(ge=1), db: Session = Depends(get_db)):
     third_party_service = ThirdPartyService()
     third_party = third_party_service.get_third_party_by_id(third_party_id, db)
     return third_party
 
 
-@third_party_router.get("/user/{user_id}", response_model=ThirdPartyResponseDTO)
+@third_party_router.get("/user/{user_id}", response_model=ThirdPartyResponseDTO,
+                        dependencies=[Security(authorized, scopes=[PermissionEnum.READ_USER.code])])
 async def get_third_party_by_user_id(user_id: int = Path(ge=1), db: Session = Depends(get_db)):
     third_party_service = ThirdPartyService()
     third_party = third_party_service.get_third_party_by_user_id(user_id, db)
--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from app.api.v1.public import third_party_router 
+from app.api.v1.private import third_party_router 
 
-public_router = APIRouter(prefix="/additional_service")
-public_router.include_router(third_party_router)
+private_router = APIRouter(prefix="/additional_service")
+private_router.include_router(third_party_router)
