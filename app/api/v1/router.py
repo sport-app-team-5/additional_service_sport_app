f9618930410a61cef851cc6ca1bec4ea47b76aa6
cccortesc1 <cc.cortesc1@uniandes.edu.co>
2024-04-13 16:25:21 -0500

    Se realiza ajuste dejando sin auth el endpoint de create third party

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -1,5 +1,10 @@
 from fastapi import APIRouter
-from app.api.v1.private import third_party_router 
+from app.api.v1.private import third_party_router as private_add_service_router
+from app.api.v1.public import third_party_router as public_add_service_router
 
 private_router = APIRouter(prefix="/additional_service")
-private_router.include_router(third_party_router)
+private_router.include_router(private_add_service_router)
+
+
+public_router = APIRouter(prefix="/additional_service")
+public_router.include_router(public_add_service_router)
