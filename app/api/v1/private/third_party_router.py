0851c54a1a14ace0ace34e320fec4a24f83c43b0
Luis Fernando santa <l.santa@uniandes.edu.co>
2024-04-13 17:23:42 -0500

    add get jwt user

--- app/api/v1/private/third_party_router.py
+++ app/api/v1/private/third_party_router.py
@@ -6,7 +6,7 @@ from app.modules.auth.domain.enums.permission_enum import PermissionEnum
 from app.modules.auth.domain.service import AuthService
 from app.modules.third_party.aplication.dto import ThirdPartyRequestDTO, ThirdPartyResponseDTO
 from app.modules.third_party.aplication.service import ThirdPartyService
-from app.seedwork.presentation.jwt import oauth2_scheme
+from app.seedwork.presentation.jwt import oauth2_scheme, get_current_user_id
 
 auth_service = AuthService()
 authorized = auth_service.authorized
@@ -16,13 +16,14 @@ third_party_router = APIRouter(
     dependencies=[Depends(oauth2_scheme)]
 )
 
-@third_party_router.post("", response_model=ThirdPartyResponseDTO
-                         ,dependencies=[Security(authorized, scopes=[PermissionEnum.READ_USER.code])]
-                         , status_code=status.HTTP_201_CREATED)
-async def create_third_party(third_party: ThirdPartyRequestDTO, db: Session = Depends(get_db)):
+
+@third_party_router.post("", response_model=ThirdPartyResponseDTO, status_code=status.HTTP_201_CREATED)
+async def create_third_party(third_party: ThirdPartyRequestDTO, db: Session = Depends(get_db),
+                             user_id: int = Depends(get_current_user_id)):
     third_party_service = ThirdPartyService()
     third_party_created = third_party_service.create_third_party(third_party, db)
     return third_party_created
+
 
 @third_party_router.get("", response_model=List[ThirdPartyResponseDTO],
                         dependencies=[Security(authorized, scopes=[PermissionEnum.READ_USER.code])])
